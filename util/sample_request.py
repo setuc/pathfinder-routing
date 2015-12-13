90a08449f9bc5c2a4a17f803119bd6dd7a9fb40e
Adam Michael <adam@ajmichael.net>
2015-12-13 12:52:47 -0500

    Added distances and capacities to test script.

--- util/sample_request.py
+++ util/sample_request.py
@@ -3,10 +3,33 @@
 import requests
 
 def request_route(route_url):
-  data = {'vehicles': [1, 3, 5], 'commodities': {0: 2, 4: 6}}
+  data = {
+    'vehicles': [ 1, 3, 5 ],
+    'commodities': {
+      0: 2,
+      4: 6
+    },
+    'distances': [
+      [ 0, 1, 1, 1, 1, 1, 1 ],
+      [ 1, 0, 1, 1, 1, 1, 1 ],
+      [ 1, 1, 0, 1, 1, 1, 1 ],
+      [ 1, 1, 1, 0, 1, 1, 1 ],
+      [ 1, 1, 1, 1, 0, 1, 1 ],
+      [ 1, 1, 1, 1, 1, 0, 1 ],
+      [ 1, 1, 1, 1, 1, 1, 0 ],
+    ],
+    'capacities': [
+      { 0: 1,
+        1: 1,
+        3: 1,
+        4: 1,
+        5: 1
+      }
+    ]
+  }
   r = requests.post(route_url, json=data)
   print 'Status code: ' + str(r.status_code)
-  print 'Response json: ' + r.json()
+  print 'Response json: ' + str(r.json())
   
 
 if __name__ == '__main__':
