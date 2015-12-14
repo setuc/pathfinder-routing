0c4d05ba6b06ea38a9984f36b44d2e953f5065a2
Adam Michael <adam@ajmichael.net>
2015-12-14 00:02:14 -0500

    Updated output

--- src/server.jl
+++ src/server.jl
@@ -14,7 +14,7 @@ function startserver()
     vehicles = jsonreq["vehicles"]
     distances = transpose(hcat(jsonreq["distances"]...))
     result = optimize(jsonreq["vehicles"], jsonreq["commodities"], distances, jsonreq["capacities"])
-    response = JSON.json(Dict("route" => result))
+    response = JSON.json(Dict("routes" => result))
     info("Returning response: ", response)
     return Response(response)
   end
