57f5eaa499f4ddfd30f59345923211d1ec2d3b7f
Adam Michael <adam@ajmichael.net>
2015-12-13 20:55:34 -0500

    Made test deterministic.

--- test/solve.jl
+++ test/solve.jl
@@ -4,13 +4,13 @@ using PathfinderRouting
 vehicles = [ 1, 3, 5 ]
 commodities = Dict('7' => 2, '4' => 6)
 distances = []
-push!(distances, [0,1,1,1,9,1,1])
-push!(distances, [9,0,1,1,9,1,1])
-push!(distances, [9,1,0,1,9,1,1])
-push!(distances, [9,1,1,0,9,1,1])
-push!(distances, [9,1,1,1,0,1,1])
-push!(distances, [9,1,1,1,9,0,1])
-push!(distances, [9,1,1,1,9,1,0])
+push!(distances, [0,9,0,9,0,9,9])
+push!(distances, [9,0,9,1,9,9,9])
+push!(distances, [0,9,0,9,0,9,1])
+push!(distances, [9,9,9,0,9,1,9])
+push!(distances, [0,9,0,9,0,9,9])
+push!(distances, [9,9,9,9,9,0,9])
+push!(distances, [9,1,9,9,9,9,0])
 capacities = []
 push!(capacities, Dict(1 => 1, 3 => 1, 4 => 1, 5 => 1, 7 => 1))
 
@@ -19,6 +19,6 @@ push!(expected, [1])
 push!(expected, [3,7,2,4,6])
 push!(expected, [5])
 
-route = PathfinderRouting.optimize(vehicles, commodities, hcat(distances...), capacities)
+route = PathfinderRouting.optimize(vehicles, commodities, transpose(hcat(distances...)), capacities)
 
 @test route == expected
