1096c565d7324b8177c8f049b145d5766e0cec71
Adam Michael <adam@ajmichael.net>
2015-12-13 20:24:00 -0500

    Added unit test.

--- test/solve.jl
+++ test/solve.jl
@@ -1,4 +1,24 @@
 using Base.Test
 using PathfinderRouting
 
-@test PathfinderRouting.solve("input data") == "Just kidding I don't work yet."
+vehicles = [ 1, 3, 5 ]
+commodities = Dict('7' => 2, '4' => 6)
+distances = []
+push!(distances, [0,1,1,1,9,1,1])
+push!(distances, [9,0,1,1,9,1,1])
+push!(distances, [9,1,0,1,9,1,1])
+push!(distances, [9,1,1,0,9,1,1])
+push!(distances, [9,1,1,1,0,1,1])
+push!(distances, [9,1,1,1,9,0,1])
+push!(distances, [9,1,1,1,9,1,0])
+capacities = []
+push!(capacities, Dict(1 => 1, 3 => 1, 4 => 1, 5 => 1, 7 => 1))
+
+expected = []
+push!(expected, [1])
+push!(expected, [3,7,2,4,6])
+push!(expected, [5])
+
+route = PathfinderRouting.optimize(vehicles, commodities, hcat(distances...), capacities)
+
+@test route == expected
