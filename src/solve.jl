f1982e503e9ffef9a227dfa873ac20f1f9e5c8e7
Adam Michael <adam@ajmichael.net>
2016-01-26 14:37:07 -0500

    Fixed floating point timestamp bug.

--- src/solve.jl
+++ src/solve.jl
@@ -15,10 +15,10 @@ function routecalculation(transports, commodities, distances, durations, capacit
 
   model = Model()
 
-  @defVar(model, pickup_time[DA], Int)
-  @defVar(model, dropoff_time[DA], Int)
-  @defVar(model, distance[RA], Int)
-  @defVar(model, duration[TA], Int)
+  @defVar(model, pickup_time[DA])
+  @defVar(model, dropoff_time[DA])
+  @defVar(model, distance[RA] >= 0, Int)
+  @defVar(model, duration[TA] >= 1, Int)
 
   # x[k1,k2,i] = transport i's path contains an edge from k1 to k2
   @defVar(model, x[RA,RA,TA], Bin)
@@ -126,9 +126,11 @@ function routecalculation(transports, commodities, distances, durations, capacit
   end
 
   for k1 in CA
-    @addConstraint(model, sum{y[k3,k1,i], i=TA, k3=RA} >= 1)
+    @addConstraint(model, sum{y[i,k1,i], i=TA} >= 1)
     for k2 in RA
-      @addConstraint(model, sum{y[k1,k2,i] + y[k2,k1,i], i=TA} <= 1)
+      if k1 != k2
+        @addConstraint(model, sum{y[k1,k2,i] + y[k2,k1,i], i=TA} <= 1)
+      end
     end
   end
 
@@ -141,16 +143,6 @@ function routecalculation(transports, commodities, distances, durations, capacit
         if k2 != i
           # A transport cannot be in another transport's route.
           @addConstraint(model, x[k2, k1, i] == 0)
-        end
-      end
-    end
-  end
-
-  for k1 in TA
-    for i in TA
-      if k1 != i
-        for k2 in RA
-          @addConstraint(model, x[k1,k2,i] == 0)
         end
       end
     end
